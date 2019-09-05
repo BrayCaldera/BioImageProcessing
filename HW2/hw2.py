b416a160fb812dc3c6f815367307d0f220c08c6f
Daniel Caldera <bdcaldera@gmail.com>
2019-09-05 00:14:27 -0500

    Se Agregó una nueva image

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -4,31 +4,33 @@ import numpy as np
 
 def brillo(imagen,valor_intensidad):
 
-    for i in range(0,imagen.shape[0]):
-        for j in range(0,imagen.shape[1]):
-            imagen[i,j] += valor_intensidad
-
+    for i in range(0, imagen.shape[0]):
+        for j in range(0, imagen.shape[1]):
+            a = imagen[i,j]
+            b = a + valor_intensidad
+            if b > 255:
+                b = 255
+            imagen[i,j] = b
 
     return imagen
 
 def print_resultados(imagen,imagen_brillo):
     print('*--------*--------*--------*--------*--------*--------*--------*')
-    print('Imagen original: (x={}, y={})~R:{}, G:{}, B:{}'.format(50,150,imagen[50,150,2]
-    ,imagen[50,150,1],imagen[50,150,0]))
+    print('Imagen original: (x={}, y={})~L:{}'.format(50,150,imagen[50,150]))
     print('*--------*--------*--------*--------*--------*--------*--------*')
-    print('Imagen Brillo: (x={}, y={})~R:{}, G:{}, B:{}'.format(50,150,imagen_brillo[50,150,2]
-    ,imagen_brillo[50,150,1],imagen_brillo[50,150,0]))
+    print('Imagen Brillo: (x={}, y={})~L:{}'.format(60,160,imagen_brillo[60,160]))
 
 def run():
-    imagen = cv2.imread('watch.jpg')
+    imagen = cv2.imread(str(input('Ingresa el nombre de la imagen: '))
+    , cv2.IMREAD_GRAYSCALE)
     cv2.imshow('Imagen Original',imagen)
-    imagen_brillo = brillo(imagen,2)
+    imagen_brillo = brillo(imagen,50)
     cv2.imshow('Imagen con Brillo',imagen_brillo)
 
     print_resultados(imagen,imagen_brillo)
 
-    if cv2.waitKey(0) == True:
-        cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__=='__main__':
     run()
