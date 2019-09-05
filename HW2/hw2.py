918bf2344be1db80b93c56b73d796a9ca6a97745
Daniel Caldera <bdcaldera@gmail.com>
2019-09-04 22:54:59 -0500

    Se avanzo en el uso de las funciones

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,35 +1,31 @@
 import cv2
 import matplotlib
 import numpy as np
-import time
 
 def brillo(imagen,valor_intensidad):
-    tiempoIn = time.time()
 
-    imagen_brillo=imagen
-    for i in range(0,imagen_brillo.shape[0]):
-        for j in range(0,imagen_brillo.shape[1]):
-            imagen_brillo[i,j] += valor_intensidad
+    for i in range(0,imagen.shape[0]):
+        for j in range(0,imagen.shape[1]):
+            imagen[i,j] += valor_intensidad
 
-    tiempoFin = time.time()
 
+    return imagen
+
+def print_resultados(imagen,imagen_brillo):
     print('*--------*--------*--------*--------*--------*--------*--------*')
-    print('Imagen Original: (x={}, y={})~R:{}, G:{}, B:{}'.format(50,150,imagen[50,150,2]
+    print('Imagen original: (x={}, y={})~R:{}, G:{}, B:{}'.format(50,150,imagen[50,150,2]
     ,imagen[50,150,1],imagen[50,150,0]))
-    print('*--------*--------*--------*--------*--------*--------*--------*')
-    print('El Proceso Tardo: ', tiempoFin - tiempoIn, 'Segundos')
-    print('*--------*--------*--------*--------*--------*--------*--------*')
-    print('Ejemplo de los valores que cambiaron con la función brillo')
     print('*--------*--------*--------*--------*--------*--------*--------*')
     print('Imagen Brillo: (x={}, y={})~R:{}, G:{}, B:{}'.format(50,150,imagen_brillo[50,150,2]
     ,imagen_brillo[50,150,1],imagen_brillo[50,150,0]))
 
-    cv2.imshow('Imagen con Brillo',imagen_brillo)
-
 def run():
     imagen = cv2.imread('watch.jpg')
     cv2.imshow('Imagen Original',imagen)
-    brillo(imagen,1)
+    imagen_brillo = brillo(imagen,2)
+    cv2.imshow('Imagen con Brillo',imagen_brillo)
+
+    print_resultados(imagen,imagen_brillo)
 
     if cv2.waitKey(0) == True:
         cv2.destroyAllWindows()
