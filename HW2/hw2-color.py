ffa6b832303d3d5dfa38d3cb35f603ac3c051e75
Daniel Caldera <bdcaldera@gmail.com>
2019-09-10 11:55:58 -0500

    se modifico el programa hw2-color.py

--- HW2/hw2-color.py
+++ HW2/hw2-color.py
@@ -3,7 +3,7 @@
 import cv2
 
 def brillo_azul(imagen,valor_intensidad):
-    """Función para aumentar el brillo de una imagen BW pixel por pixel"""
+    """Función para aumentar el brillo de un canal pixel por pixel"""
     for i in range(0, imagen.shape[0]):
         for j in range(0, imagen.shape[1]):
             a = imagen[i,j][0]
@@ -15,10 +15,28 @@ def brillo_azul(imagen,valor_intensidad):
     return imagen
 
 def brillo_verde(imagen,valor_intensidad):
-    pass
+    """Función para aumentar el brillo de un canal pixel por pixel"""
+    for i in range(0, imagen.shape[0]):
+        for j in range(0, imagen.shape[1]):
+            a = imagen[i,j][1]
+            b = a + valor_intensidad
+            if b > 255:
+                b = 255
+            imagen[i,j][1] = b
+
+    return imagen
 
 def brillo_rojo(imagen,valor_intensidad):
-    pass
+    """Función para aumentar el brillo de un canal pixel por pixel"""
+    for i in range(0, imagen.shape[0]):
+        for j in range(0, imagen.shape[1]):
+            a = imagen[i,j][2]
+            b = a + valor_intensidad
+            if b > 255:
+                b = 255
+            imagen[i,j][2] = b
+
+    return imagen
 
 def run():
     imagen = cv2.imread('images/'+str(input('Ingresa el nombre de la imagen: ')))
@@ -30,12 +48,13 @@ def run():
     intensidad_canal_rojo = int(input('¿Cuánto brillo deseas en el canal rojo?: '))
 
     imagen_brillo_azul = brillo_azul(imagen,intensidad_canal_azul)
-    imagen_brillo_azul = brillo_verde(imagen,intensidad_canal_azul)
-    imagen_brillo_azul = brillo_rojo(imagen,intensidad_canal_azul)
+    imagen_brillo_verde = brillo_verde(imagen,intensidad_canal_azul)
+    imagen_brillo_rojo = brillo_rojo(imagen,intensidad_canal_azul)
 
-    cv2.imwrite('images/'+str(
-        input('¿Con cuál nombre quieres guardar las nuevas imagenes?: '))+
-        '.jpg', imagen_brillo_azul)
+    nombre_image_nueva=input('¿Con cuál nombre quieres guardar las nuevas imagenes?: ')
+    cv2.imwrite('images/'+nombre_image_nueva+'-azul.jpg', imagen_brillo_azul)
+    cv2.imwrite('images/'+nombre_image_nueva+'-verde.jpg', imagen_brillo_verde)
+    cv2.imwrite('images/'+nombre_image_nueva+'-rojo.jpg', imagen_brillo_rojo)
 
 if __name__=='__main__':
     run()
