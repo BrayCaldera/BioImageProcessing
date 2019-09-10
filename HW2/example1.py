1dba1b41ad21b5fa629f407516a2a2e0e2bbf653
Daniel Caldera <bdcaldera@gmail.com>
2019-09-10 13:36:09 -0500

    Se actualizo hw2-color

--- HW2/example1.py
+++ HW2/example1.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #img = cv2.imread('watch.jpg',cv2.IMREAD_GRAYSCALE)
-img = cv2.imread('watch.jpg',cv2.IMREAD_COLOR)
+img = cv2.imread('images/watch.jpg',cv2.IMREAD_COLOR)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
