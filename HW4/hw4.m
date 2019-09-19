d0e184b78319770bb34313d0744600b8e1a699cd
Daniel Caldera <bdcaldera@gmail.com>
2019-09-19 11:09:14 -0500

    #002

--- HW4/hw4.m
+++ HW4/hw4.m
@@ -1,15 +1,24 @@
 clc; clear all; close all;
 
 dicom_image = dicomread('image-000010.dcm');
-%figure
-%imshow(X,[]);
+figure
+imshow(dicom_image,[]);
 
 [M,I] = max(dicom_image(:));
 
-histogram_vector=zeros(1,M);
+histogram_vector=zeros(1,M+1);
+rango_vector = linspace(0,double(M),double(M)+1);
 
 for i = 1:length(dicom_image)
     for j = 1:length(dicom_image)
-        
+        pixel = dicom_image(i,j);
+        for x = 1:length(rango_vector)
+            if (pixel==rango_vector(x))
+               histogram_vector(x)=histogram_vector(x)+1; 
+            end
+        end
     end
 end
+
+figure
+bar(rango_vector,histogram_vector)
