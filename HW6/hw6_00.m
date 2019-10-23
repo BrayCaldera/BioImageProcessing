2f58c5eb19119f5c1815893bcfe0d314f0b964da
Daniel Caldera <bdcaldera@gmail.com>
2019-10-23 11:46:49 -0500

    Hw6stable01

--- HW6/hw6_00.m
+++ HW6/hw6_00.m
@@ -2,23 +2,13 @@
 % Procesamiento de Bioimágenes
 % 
 % Tarea 6
-%% EXAMPLE CODE
-
-% Notes:
-% INTENSITY_COEF=0.11353517; % 255/max(value of a particular set of DICOM)
-
-% ShowDicomSeries('series-000000/image-',178, 0, 255) % Just One
-
-% ShowDicomSeries('series-000000/image-',0:360, 0, 255) % All
-
-%--------------------------------------------------------------------------
 %% MAIN CODE
-% tic
-V = BuildMedicalVolume('series-000000/image-',0:360);
-% toc
-%%
 tic
-% ShowCrossSection(V,3,0) % Show Coronal Axis
-ShowCrossSection(V,2,0) % Show Sagital Axis
+
+V = BuildMedicalVolume('series-000000/image-',0:360);
+
+% ShowCrossSection(V,1,false) % Show Axial Axis
+% ShowCrossSection(V,3,false) % Show Coronal Axis
+ShowCrossSection(V,2,false) % Show Sagital Axis
 
 toc
